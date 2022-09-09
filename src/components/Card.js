f40c95a8f42a1f1106de02e9d0f1255d3c85143c
wramadhan <wahyuramadhan0549@gmail.com>
2022-09-09 23:34:23 +0700

    menambah page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { FaShoppingBag } from "react-icons/fa";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div>
+      <FaShoppingBag className="w-52 text-orange-500" />
+      <img src="http://placehold.it/500x300" alt="placeholder" />
+      <p>Title</p>
+    </div>
+  );
 };
 
 export default Card;
