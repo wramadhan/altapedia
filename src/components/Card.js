fb1ee26ec8654b76921cb2d960324c5413983ba2
Yoga Sulistyo <kgsyoga1997@gmail.com>
2022-09-10 16:18:30 +0700

    buat card product

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,11 @@ import { FaShoppingBag } from "react-icons/fa";
 
 const Card = () => {
   return (
-    <div>
-      <FaShoppingBag className="w-52 text-orange-500" />
-      <img src="http://placehold.it/500x300" alt="placeholder" />
-      <p>Title</p>
+    <div className="w-72 h-96">
+      <img className="rounded-lg h-64 w-72" src="https://ncrsport.com/img/storage/large/FU7337-1.jpg" alt="placeholder" />
+      <h2 className="font-semibold text-lg text-[#1B345F] pt-2">Sepatu Adidas</h2>
+      <h3 className="font-semibold text-lg text-[#1B345F] pb-2">Rp.1.750.000</h3>
+      <button className="rounded-lg font-medium text-lg w-72 h-12 bg-[#FEF3EB] text-[#F7731C]">Add to cart</button>
     </div>
   );
 };
