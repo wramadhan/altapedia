e83f0382714e86991ded7b138579961fd49e1a35
wramadhan <wahyuramadhan0549@gmail.com>
2022-09-10 20:11:13 +0700

    jumbotron telah selesai

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "../images/jumbotronimg.png";
+
+const Jumbotron = () => {
+  return (
+    <div className="sm:px-20 pt-20 sm:flex justify-between">
+      <div>
+        <h1 className="text-[#F7731C] font-bold text-3xl mb-5 md:text-4xl text-center sm:text-left sm:pt-20">
+          Everyone’s<span> </span>
+          <span className="sm:hidden text-[#1B345F] font-bold text-3xl mb-5 text-center">
+            Collections <span className="text-[#F7731C]">and</span> Style
+          </span>
+        </h1>
+        <h1 className="invisible sm:visible sm:text-[#1B345F] font-bold text-3xl mb-5 md:text-4xl text-center sm:text-left">
+          Collections <span className="sm:text-[#F7731C]">and</span> Style
+        </h1>
+        <p className="sm:mt-6 text-[#1B345F] font-medium text-md sm:text-lg text-center sm:text-left">
+          A collection of clothes with contemporary styles{" "}
+          <br className="visible sm:invisible" />
+          and trends that make you look even cooler
+        </p>
+        <div className="flex justify-center">
+          <button className="my-5 sm:my-12 bg-[#F7731C] text-white font-semibold px-1 sm:py-3 sm:px-10 rounded-full sm:rounded-lg text-base">
+            Explore More
+          </button>
+        </div>
+      </div>
+      <div>
+        <div className="mt-10 flex justify-center">
+          <img
+            className=" w-1/2 sm:w-56 md:w-80 lg:w-96"
+            src={Image}
+            alt="horse"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Card from '../components/Card'
 import Footer from '../components/Footer'
+import Jumbotron from '../components/Jumbotron'
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <Jumbotron />
             <div>Home</div>
             <Card />
             <Footer />
